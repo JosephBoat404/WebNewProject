aba331ec9985f8c984496fe480a61c80e2efa163
josephboat404 <josephboatengcollege45@gmail.com>
2023-07-11 01:32:13 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let element = document.getElementById('Joe2');
+ 
+function turnBlue() {
+    let colorsele=['red','blue','green']
+    let rand1=Math.floor(Math.random()*4)+0;
+    console.log(rand1);
+
+
+    element.style.color=colorsele[rand1];
+
+}
+element.addEventListener('click',turnBlue)
+
+element.onmouseover = function() {
+    this.style.cursor = "pointer";
+  };
+  
+function turnNormal(){
+    element.style.color='#0F7173';
+}
+element.addEventListener('mouseout',turnNormal)
+
